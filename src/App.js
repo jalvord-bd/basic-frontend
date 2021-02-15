7111af1b94de9be390a3aed8f0ee3d8291c4307e
jalvord-bd <jake.alvord@beam.dental>
2021-02-15 18:51:58 -0500

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const response = axios
+	.get('https://0.0.0.0:3030/api/v1/basic/1')
+  	.then(response => console.log(response))
+
   return (
     <div className="App">
       <header className="App-header">
